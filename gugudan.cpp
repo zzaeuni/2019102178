867bcc5462ce3875f67d72574b130b0e89d90069
qkrwodbsfjq@khu.ac.kr <qkrwodbsfjq@khu.ac.kr>
2020-10-16 19:06:22 +0900

    add 9 dan

--- gugudan.cpp
+++ gugudan.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 	int n = 9;
-	for (int i = 1; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++)
 			cout << i << " * " << j << " = " << i * j << endl;
 	}
