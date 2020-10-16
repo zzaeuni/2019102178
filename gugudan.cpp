6cc65d3319dff4c6b80b9091bd313d6fb888e55f
qkrwodbsfjq@khu.ac.kr <qkrwodbsfjq@khu.ac.kr>
2020-10-16 18:44:07 +0900

    adding gugudan.cpp

---
+++ gugudan.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n = 9;
+	for (int i = 1; i < n; i++) {
+		for (int j = 1; j <= n; j++)
+			cout << i << " * " << j << " = " << i * j << endl;
+	}
+	return 0;
+}
